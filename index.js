a9be068c8d3cc045096f6283ab9d4ffc257d1d20
SbabalweRoux <sbaviaroux@gmail.com>
2024-03-22 16:54:59 +0200

    Added an event for a value not entered in Chall..1

--- index.js
+++ index.js
@@ -5,7 +5,10 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(`The user prefers a ` + userPreference + ' Monster Energy Drink');
+
+console.log(userPreference === 'Sugar-free' ? (`The user prefers a ` + userPreference + ' Monster Energy Drink') : "The user prefers a Regular Monster Energy Drink");
+
+
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
